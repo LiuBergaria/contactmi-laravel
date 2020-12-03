5718ba41bb384107bf2007ca8fecd66d0d033ce8
Helil Bergaria <liubergaria@hotmail.com>
2020-12-03 14:48:45 -0300

    Criação do conteúdo de home

--- resources/views/app/home.blade.php
+++ resources/views/app/home.blade.php
@@ -1,8 +1,16 @@
 @extends('layouts.app')
 
+<?php
+    setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+    date_default_timezone_set('America/Sao_Paulo');
+?>
+
 @section('content')
-<div id="home" class="d-none d-md-block">
-    <p>Home</p>
+<div id="home" class="d-none d-md-block text-center">
+    <div>
+        <h2>Seja bem vindo, {{ Auth::user()->name }}!</h2>
+        <span>{{ ucfirst(strftime('%A, dia %d de %B de %Y', strtotime('today'))) }}</span>
+    </div>
 </div>
 @include('app.contacts-list', ['class' => 'd-md-none mobile'])
 @endsection
