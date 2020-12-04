784c0f20ec033478943a0e0c4abec7c22b450490
Helil Bergaria <liubergaria@hotmail.com>
2020-12-04 11:12:13 -0300

    Gerado migrations, controllers e models

---
+++ database/migrations/2020_12_04_140911_create_contacts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->string('name');
+            $table->boolean('fg_favorite')->default(false);
+            $table->string('cep', 8)->nullable();
+            $table->string('full_address')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
---
+++ database/migrations/2020_12_04_140933_create_contact_phones_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactPhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_phones', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_contact');
+            $table->string('phone');
+            $table->string('description')->nullable();
+
+            $table->foreign('id_contact')->references('id')->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_phones');
+    }
+}
